055274e98ddf34fc98a061b9e720d67ac6fd68f2
Defy <61808223+De-y@users.noreply.github.com>
2023-10-21 20:21:10 -0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,14 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   head: {
-    title: 'sAlina Dashboard',
+    title: 'sAlia Dashboard',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: 'This is the dashboard for everything about SSSniperwolf, also known as Alina Marie Shelesh (Lia).'
+        content: 'This is the dashboard for everything about SSSniperwolf, also known as Alia Marie Shelesh (Lia).'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]

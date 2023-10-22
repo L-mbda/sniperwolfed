171a3b0a513185744d2e466eac871571f8534066
Defy <61808223+De-y@users.noreply.github.com>
2023-10-21 20:07:25 -0700

    We need your help!
    Added the field.

--- pages/index.vue
+++ pages/index.vue
@@ -33,34 +33,71 @@ function convertIsoToTime(isoDuration: string): string {
                     <p>{{ timelineItem.values["header"] }}</p>
                 </li>
             </ul>
-            
-            <!-- Gets json data of video changes & removals -->
-            <ul class="video_modifications">
-                <h1>Video Modifications</h1>
-                <li v-for="video in videoData?.jsondata">
-                    <li v-if="video.type === 'removed'">
-                        {{ video.type }}: {{ video.old_video?.snippet.title }}
-                        <p>Published on {{ new Date(video.old_video?.snippet.publishedAt).toLocaleString([], {month: 'numeric', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true}) }}</p>
-                        <p>Duration: {{ convertIsoToTime(video.old_video?.contentDetails.duration) }}</p>
-                        <p>Stats: {{ video.old_video?.statistics?.viewCount }} views, {{ video.old_video?.statistics?.likeCount }} likes, and {{ video.old_video?.statistics?.commentCount }} comments</p>
-                    </li>
-                    <li v-else>
-                        {{ video.type }}: {{ video.video?.snippet.title }}
-                        <p>Published on {{ new Date(video.video?.snippet.publishedAt).toLocaleString([], {month: 'numeric', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true}) }}</p>
-                        <p>Video: <a :href="'https://youtube.com/watch?v=' + video.video?.id" style="text-decoration: none; color: rgb(53, 158, 238);" class="hov_a">Link</a></p>
-                        <p>New Duration: {{ convertIsoToTime(video.video.contentDetails.duration) }}</p>
-                        <p>Stats at the time of capture: {{ video.video?.statistics?.viewCount }} views, {{ video.video?.statistics?.likeCount }} likes, and {{ video.video?.statistics?.commentCount }} comments</p>
-                    </li>
-                </li>
-            </ul>
-            </div> <br>
+            <div class="side-by-side">
+                <div>
+                    <!-- Gets json data of video changes & removals -->
+                    <ul class="video_modifications">
+                        <h1>Video Modifications</h1>
+                        <li v-for="video in videoData?.jsondata">
+                            <li v-if="video.type === 'removed'">
+                                {{ video.type }}: {{ video.old_video?.snippet.title }}
+                                <p>Published on {{ new Date(video.old_video?.snippet.publishedAt).toLocaleString([], {month: 'numeric', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true}) }}</p>
+                                <p>Duration: {{ convertIsoToTime(video.old_video?.contentDetails.duration) }}</p>
+                                <p>Stats: {{ video.old_video?.statistics?.viewCount }} views, {{ video.old_video?.statistics?.likeCount }} likes, and {{ video.old_video?.statistics?.commentCount }} comments</p>
+                            </li>
+                            <li v-else>
+                                {{ video.type }}: {{ video.video?.snippet.title }}
+                                <p>Published on {{ new Date(video.video?.snippet.publishedAt).toLocaleString([], {month: 'numeric', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true}) }}</p>
+                                <p>Video: <a :href="'https://youtube.com/watch?v=' + video.video?.id" style="text-decoration: none; color: rgb(53, 158, 238);" class="hov_a">Link</a></p>
+                                <p>New Duration: {{ convertIsoToTime(video.video.contentDetails.duration) }}</p>
+                                <p>Stats at the time of capture: {{ video.video?.statistics?.viewCount }} views, {{ video.video?.statistics?.likeCount }} likes, and {{ video.video?.statistics?.commentCount }} comments</p>
+                            </li>
+                        </li>
+                    </ul>
+                </div>
+                <br>
+                <div class="help">
+                    <h1>We need your help!</h1>
+                    <a href="https://docs.google.com/document/d/1_BUpaSN8ZU44LrnVsELcBVlD9c3e6TH-17LKmWw2P30/edit" style="text-decoration: none; color: rgb(1, 221, 255);">Google Doc Link</a>
+                    <p>Spread this document to anyone you know who is aware of the SSSniperwolf situation. The document contains serious incriminating evidence against Alia that was posted on her own channel.</p>
+                </div>
+            </div>
+            <br>
             <footer style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">
-                <p>Created by <a href="https://github.com/de-y" style="text-decoration: none; color: rgb(59, 255, 115);">de-y</a></p>
+                <p>Created by <a href="https://github.com/de-y" style="text-decoration: none; color: rgb(59, 255, 115);">de-y</a> with help of the TTTakedown Tracker community.</p>
                 <a href="https://github.com/de-y/sniperwolfed" style="text-decoration: none; color: rgb(0, 191, 255);">GitHub</a>
             </footer>
+        </div>
     </div>
 </template>
 <style scoped>
+    .help {
+        background-color: #343434;
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        color: white;
+        width: 50%;
+        padding: 10px;
+        border-radius: 10px;
+        height: 48vh;
+        transform: translateY(4%);
+        align-self: right;
+        text-align: center;
+        overflow: auto;
+    }
+    .help h1 {
+        font-size: 3rem;
+    }
+    .help a {
+        font-size: 2rem;
+    }
+    .help p {
+        font-size: 1rem;
+    }
+    .side-by-side {
+        display: flex;
+        flex-direction: row;
+        column-gap: 50px;
+    }
     .hov_a:hover {
         color: rgb(19, 152, 254);
         transition: 0.5s;
@@ -103,5 +140,26 @@ function convertIsoToTime(isoDuration: string): string {
         padding: 50px;
         border-radius: 10px;
         color: white;
+    }
+    @media only screen and (max-width: 600px) {
+        .timeline-item {
+            width: 100%;
+        }
+        .side-by-side {
+            flex-direction: column;
+        }
+        .help {
+            width: fit-content;
+            transform: translateY(0%);
+        }
+        .help h1 {
+            font-size: 2rem;
+        }
+        .help a {
+            font-size: 1.5rem;
+        }
+        .help p {
+            font-size: 1rem;
+        }
     }
 </style>
